4f98231257984f7adf6cbe581027106e6f485ab2
KimyoungLee <lkmdilook@gmail.com>
2012-11-20 22:03:17 +0900

    add config ui

--- app/view/Settings.js
+++ app/view/Settings.js
@@ -2,7 +2,7 @@ Ext.define('VoucherGuideForSilvers.view.Settings', {
 	extend: 'Ext.Panel',
 	xtype: 'settingsPanel',
 	
-	requires: ['Ext.TitleBar'],
+	requires: ['Ext.TitleBar', 'Ext.field.Slider', 'Ext.field.Select'],
 	
 	config: {
 		title: '설정',
@@ -14,7 +14,32 @@ Ext.define('VoucherGuideForSilvers.view.Settings', {
 		
 		items: [{
 			xtype: 'titlebar',
-			title: '설정'
-		}]
+			title: '설정',
+			docked: 'top'
+		},
+		{
+			xtype: 'panel',
+			id: 'configFormPanel',
+			flex: 1,
+			items: [
+			{
+				xtype: 'sliderfield',
+				name: 'fontSize',
+				label: '글자 크기'
+			},
+			//TODO: 이거 텍스트입력창으로교체해야됨.
+			{
+				xtype: 'selectfield',
+				name: 'address',
+				id: 'addressSelect',
+				label: '주소',
+				//store: 'sidoStore',
+				//displayField: 'sido',
+				//valueField: 'sido',
+				placeHolder: '주소 입력'
+			}
+			]
+		}
+		]
 	}
 });
