b551b8a2f9723f852dc9577dd3f411feb9b05e91
KimyoungLee <lkmdilook@gmail.com>
2012-11-22 21:37:51 +0900

    Mockup setting UI 한글되나? 설정UI 완료

--- app/view/Settings.js
+++ app/view/Settings.js
@@ -27,17 +27,47 @@ Ext.define('VoucherGuideForSilvers.view.Settings', {
 				name: 'fontSize',
 				label: '글자 크기'
 			},
-			//TODO: 이거 텍스트입력창으로교체해야됨.
+//			//TODO: 이거 텍스트입력창으로교체해야됨.
+//			{
+//				xtype: 'selectfield',
+//				//name: 'address',
+//				//id: 'addressSelect',
+//				label: '주소',
+//				//store: 'sidoStore',
+//				//displayField: 'sido',
+//				//valueField: 'sido',
+//				placeHolder: '주소 입력'
+//			},
 			{
-				xtype: 'selectfield',
-				name: 'address',
-				id: 'addressSelect',
-				label: '주소',
-				//store: 'sidoStore',
-				//displayField: 'sido',
-				//valueField: 'sido',
-				placeHolder: '주소 입력'
-			}
+                xtype: 'textfield',
+                name: 'name',
+                label: '주소',
+                placeHolder: '주소 입력',
+                autoCapitalize: false
+            },
+			//상담 전화번호
+			{
+                xtype: 'numberfield',
+                name: 'number',
+                label: '상담 전화번호',
+                placeHolder: '전화번호 입력'
+            },
+            //응급 전화번호	
+            {
+                xtype: 'numberfield',
+                name: 'number',
+                label: '응급 전화번호',
+                placeHolder: '전화번호 입력'
+            },
+            {
+            	xtype: 'button',
+            	text: '완료',
+            	id: 'settingConfirmationBtn',
+            	ui: 'confirm',
+            	//iconCls: 'settings',
+            	iconMask: true,
+            	margin: '15px'
+            }
 			]
 		}
 		]
