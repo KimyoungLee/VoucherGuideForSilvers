5ad15ed8ca3fe2d85e357bef8067f1a6af6c73d3
ninetails <ninetails@ninetails.kr>
2012-12-11 02:58:44 +0900

    상담전화번호 처리

--- app/controller/MainNavController.js
+++ app/controller/MainNavController.js
@@ -63,14 +63,37 @@ Ext.define('VoucherGuideForSilvers.controller.MainNavController', {
 		});
 	}, 
 	
-	onMainPanelActiveItemChange : function(comp, value, oldValue, eOpts) {
+	onMainPanelActiveItemChange : function(tabPanel, value, oldValue, eOpts) {
 		if (value._itemId == 'emergencyCallBtn') {
 			// 긴급통화 버튼 선택시
 			var emergencyPhoneNumber = localStorage.emergencyPhoneNumber;
 			if(!localStorage.emergencyPhoneNumber) emergencyPhoneNumber = '119';
-			window.open('tel:'+ emergencyPhoneNumber);
-
-			return;
+			document.location.href = 'tel:'+ emergencyPhoneNumber;
+		} else if (value._itemId == 'counselCallBtn') {
+			// 상담통화 버튼 선택시
+			var counselPhoneNumber = localStorage.counselPhoneNumber;
+			
+			// 등록된 상담전화번호가 없으면
+			if (!counselPhoneNumber) {
+				Ext.Msg.confirm('상담전화등록오류!',
+						'등록된 상담전화번호가 없습니다.<br />상담전화번호를 등록하시겠습니까?',
+						function(button) {
+							if (button == 'yes') {
+								// 설정페이지로 이동
+								tabPanel.animateActiveItem('settingsPanel', {
+									type: 'slide',
+									direction: 'left'
+								});
+							} else {
+								tabPanel.animateActiveItem('homePanel', {
+									type: 'slide',
+									direction: 'right'
+								});
+							}
+				});
+			} else {
+				document.location.href = 'tel:'+ counselPhoneNumber;
+			}
 		}
 	}
 });
