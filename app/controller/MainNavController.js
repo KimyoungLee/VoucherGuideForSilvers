189c1546a6f9904c881dc5d12ac7c47840e0860d
ninetails <ninetails@ninetails.kr>
2012-12-11 21:17:31 +0900

    sencha app build production

--- app/controller/MainNavController.js
+++ app/controller/MainNavController.js
@@ -1,6 +1,6 @@
 Ext.define('VoucherGuideForSilvers.controller.MainNavController', {
 	extend : 'Ext.app.Controller',
-	
+
 	config: {
 		control : {
 			settingsBtn : {
@@ -27,55 +27,55 @@ Ext.define('VoucherGuideForSilvers.controller.MainNavController', {
 			counselCallBtn: '#counselCallBtn'
 		}
 	},
-	
+
 	onSettingsBtnTap: function(button, e, options) {
 		button.up('main').animateActiveItem('settingsPanel', {
 			type: 'slide',
 			direction: 'left'
 		});
 	},
-	
+
 	onNewsListBtnTap: function(button, e, options) {
 		button.up('main').animateActiveItem('newsListPanel', {
 			type: 'slide',
 			direction: 'left'
 		});
 	},
-	
+
 	onInfoListBtnTap: function(button, e, options) {
 		button.up('main').animateActiveItem('infoListPanel', {
 			type: 'slide',
 			direction: 'left'
 		});
 	},
-	
+
 	onSearchBtnTap: function(button, e, options) {
 		button.up('main').animateActiveItem('searchPanel', {
 			type: 'slide',
 			direction: 'left'
 		});
 	},
-	
+
 	onrRlativeSiteBtnTap: function(button, e, options) {
 		button.up('main').animateActiveItem('relativeSitePanel', {
 			type: 'slide',
 			direction: 'left'
 		});
-	}, 
-	
+	},
+
 	onMainPanelActiveItemChange : function(tabPanel, value, oldValue, eOpts) {
 		if (value._itemId == 'emergencyCallBtn') {
 			// 긴급통화 버튼 선택시
 			var emergencyPhoneNumber = localStorage.emergencyPhoneNumber;
 			if(!localStorage.emergencyPhoneNumber) emergencyPhoneNumber = '119';
-			
+
 			Ext.Msg.confirm('응급전화연결',
 					emergencyPhoneNumber +' 로 연결하시겠습니까?',
 					function(button) {
 						if (button == 'yes') {
 							document.location.href = 'tel:'+ emergencyPhoneNumber;
 						}
-						
+
 						tabPanel.animateActiveItem('homePanel', {
 							type: 'slide',
 							direction: 'right'
@@ -84,7 +84,7 @@ Ext.define('VoucherGuideForSilvers.controller.MainNavController', {
 		} else if (value._itemId == 'counselCallBtn') {
 			// 상담통화 버튼 선택시
 			var counselPhoneNumber = localStorage.counselPhoneNumber;
-			
+
 			// 등록된 상담전화번호가 없으면
 			if (!counselPhoneNumber) {
 				Ext.Msg.confirm('상담전화등록오류!',
@@ -110,7 +110,7 @@ Ext.define('VoucherGuideForSilvers.controller.MainNavController', {
 							if (button == 'yes') {
 								document.location.href = 'tel:'+ counselPhoneNumber;
 							}
-							
+
 							tabPanel.animateActiveItem('homePanel', {
 								type: 'slide',
 								direction: 'right'
