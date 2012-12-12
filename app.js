f4fc3142d59524760ac3e9cb3fd80c3b7e22c67b
ninetails <ninetails@ninetails.kr>
2012-12-12 16:10:21 +0900

    phonegap으로 apk 생성

--- app.js
+++ app.js
@@ -49,22 +49,25 @@ Ext.application({
 	],
 
     icon: {
-        '57': 'resources/icons/Icon.png',
-        '72': 'resources/icons/Icon~ipad.png',
-        '114': 'resources/icons/Icon@2x.png',
-        '144': 'resources/icons/Icon~ipad@2x.png'
+        '57': 'resources/icons/icon_57.png',
+        '72': 'resources/icons/icon_72.png',
+        '114': 'resources/icons/icon_114.png',
+        '144': 'resources/icons/icon_144.png'
     },
 
     isIconPrecomposed: true,
 
     startupImage: {
-        '320x460': 'resources/startup/320x460.jpg',
-        '640x920': 'resources/startup/640x920.png',
-        '768x1004': 'resources/startup/768x1004.png',
-        '748x1024': 'resources/startup/748x1024.png',
-        '1536x2008': 'resources/startup/1536x2008.png',
-        '1496x2048': 'resources/startup/1496x2048.png'
+        '320x460': 'resources/startup/main.jpg',
+        '640x920': 'resources/startup/main.png',
+        '768x1004': 'resources/startup/main.png',
+        '748x1024': 'resources/startup/main.png',
+        '480x854': 'resources/startup/main.png',
+        '1536x2008': 'resources/startup/main.png',
+        '1496x2048': 'resources/startup/main.png'
     },
+    
+    phoneStartupScreen: 'resources/startup/main.png',
 
     launch: function() {
         // Destroy the #appLoadingIndicator element
